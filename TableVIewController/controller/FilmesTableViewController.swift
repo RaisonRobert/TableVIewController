7d52dd653b29c50f702d344c2fadcad4caaaa24b
Raison Robert Monteiro de Sá <raison.robert@pucpr.edu.br>
2022-09-16 17:17:06 -0400

    lista de filmes

--- TableVIewController/controller/FilmesTableViewController.swift
+++ TableVIewController/controller/FilmesTableViewController.swift
@@ -23,23 +23,24 @@ class FilmesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return filmes.lista.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "filminho", for: indexPath)
 
         // Configure the cell...
+        cell.textLabel?.text = filmes.lista[indexPath.row]
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
