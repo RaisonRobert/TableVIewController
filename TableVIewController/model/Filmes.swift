a3095f230c58c2584d0ba8d5cb0dee6531034867
Raison Robert Monteiro de Sá <raison.robert@pucpr.edu.br>
2022-09-15 22:26:09 -0400

    Init TableViewController

---
+++ TableVIewController/model/Filmes.swift
@@ -0,0 +1,21 @@
+//
+//  Filmes.swift
+//  TableVIewController
+//
+//  Created by Samel PS on 15/09/22.
+//
+
+import Foundation
+
+class Filmes{
+    var lista = [String]()
+    init(){
+        lista.append("Batman Dark Knight")
+        lista.append("A Viagem de Chihiro")
+        lista.append("Lord of the Rings")
+        lista.append("Logan")
+        lista.append("La La Land")
+        lista.append("Everything, Everywhere, All at Once")
+        lista.append("Parasita")
+    }
+}
